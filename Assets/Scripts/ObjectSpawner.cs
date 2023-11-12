533f22b70779b156c387f685e9c1fd54f1997e73
Daniel He <tcdanielh@berkeley.edu>
2023-11-12 05:13:08 -0800

    Revert "Revert "Merge branch 'main' of https://github.com/Xiaowen2024/Studio-Y""
    This reverts commit 4c547afb6487f7a1fa08a21ecc02b5ea5b5f73cb.

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public List<GameObject> objects;
+
+    public void Spawn(string objectName)
+    {
+        foreach (var item in objects)
+        {
+            item.SetActive(objectName == item.name);
+        }
+    }
+}
